90917908b67cfdafb2ff87fbbd850e6ad36fd4e7
yardenb25 <62344273+yardenb25@users.noreply.github.com>
2020-04-29 11:15:19 -0400

    Assignment 2 Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## x <<- y creates x, which is then retrieved by get(). Note that get is also a base function and is being overwritten here. 
+I would advocate in avoiding this, even though it's confined to a function. An ad hoc solution would be to reach base get function through 
+base::get.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+j <- NULL
+set <- function(y) {
+  x <<- y
+  j <<- NULL
+}
+get <- function()x
+setInverse <- function(inverse)j <<- inverse
+getInverse <- function()j
+list(set = set, get = get,
+     setInverse = setInverse,
+     getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## In this code I return the matrix which is the inverse of x using getInverse
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        j <- x$getInverse()
+        if(!is.null(j)){
+        message("getting cached data")
+        return(j)
+        }
+        mat <- x$get()
+        j <- solve(mat, ...)
+        x$setInverse(j)
 }
